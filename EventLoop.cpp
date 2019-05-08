6589c681eaa784f8d5716a46caa2ee5b32c7ee1a
zhen3410 <867792619@qq.com>
2019-05-08 13:20:50 +0800

    Update EventLoop.cpp

--- EventLoop.cpp
+++ EventLoop.cpp
@@ -17,7 +17,8 @@ const int kPollTimeMs=10000;
 
 EventLoop::EventLoop()
     :looping_(false),
-    threadId_(pthread_self())
+    threadId_(pthread_self())，
+    poller_(new Poller(this))
 {
     if(t_loopInThisThread){
         std::cerr<<"another thread exist"<<std::endl;
