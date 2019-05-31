f3a43d2ad84570dcdc939caaf96541245a99a65c
zhen3410 <867792619@qq.com>
2019-05-31 14:59:16 +0800

    test EventLoopThread

--- myServer/main.cpp
+++ myServer/main.cpp
@@ -1,31 +1,16 @@
 #include"EventLoop.h"
+#include"EventLoopThread.h"
 #include<iostream>
 
-EventLoop* g_loop;
-int flag=0;
-
-void run3(){
-	std::cout<<"run3() pid = "<<getpid()<<" , flag = "<<flag<<std::endl;
-	g_loop->quit();
-}
-
-void run2(){
-	std::cout<<"run2() pid = "<<getpid()<<" , flag = "<<flag<<std::endl;
-	flag=2;
-	g_loop->runInLoop(run3);
-}
-
-void run1(){
-	std::cout<<"run1() pid = "<<getpid()<<" , flag = "<<flag<<std::endl;
-	flag=1;
-	g_loop->runInLoop(run2);
+void runInThread(){
+	std::cout<<"runInThrad(): pid = "<<getpid()<<" , tid = "<<CurrentThread::tid()<<std::endl;
 }
 
 int main(){
-	std::cout<<"main() pid = "<<getpid()<<" , flag = "<<flag<<std::endl;
-	EventLoop loop;
-	g_loop=&loop;
-	g_loop->runInLoop(run1);
-	loop.loop();
+	std::cout<<"main() pid = "<<getpid()<<" , tid = "<<CurrentThread::tid()<<std::endl;
+	EventLoopThread loopThread;
+	EventLoop* loop=loopThread.startLoop();
+	loop->runInLoop(runInThread);
+	loop->quit();
 	std::cout<<"main() pid = "<<getpid()<<" , flag = "<<flag<<std::endl;
 }
