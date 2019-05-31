2ea16c1e0d8e0339f9e02f1b159db9ac4435bdd1
zhen3410 <867792619@qq.com>
2019-05-31 13:52:37 +0800

    add timingWheel support ing..

--- myServer/TimingWheel.h
+++ myServer/TimingWheel.h
@@ -2,18 +2,31 @@
 #define SERVER_TIMINGWHEEL_H
 
 #include"TcpConnection.h"
+#include"base/MutexLock.h"
+#include"base/Condition.h"
+#include"base/Thread.h"
+#include"base/CountDownLatch.h"
 
 #include<unordered_set>
 #include<memory>
 #include<deque>
+#include<functional>
+
+class EventLoop;
 
 class TimingWheel
 {
 public:
+    typedef std::weak_ptr<TcpConnection> weakPtrTcpConnection;
     TimingWheel(/* args */);
     ~TimingWheel();
+
+    void start();
+
+    void addConnection(weakPtrTcpConnection);
+
 private:
-    typedef std::weak_ptr<TcpConnection> weakPtrTcpConnection;
+    void threadFunc();
     /* data */
     struct Entry
     {
@@ -30,10 +43,17 @@ private:
 
         weakPtrTcpConnection weakConn_;
     };
+      
     typedef std::shared_ptr<Entry> EntryPtr;
     typedef std::weak_ptr<EntryPtr> WeakEntryPtr;
     typedef std::unordered_set<WeakEntryPtr> Bucket;
     typedef std::deque<Bucket> TimingWheelQueue;
+
+    TimingWheelQueue timingWheel_;
+    Thread thread_;
+    CountDownLatch latch_;
+    MutexLock mutex_;
+    Condition cond_;
 };
 
 
